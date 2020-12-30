24b48e5eb9530cd98b860e9eaf3bdfa0f49c53cd
Sergio <sfernandes@o-pitblast.com>
2020-12-30 19:51:42 +0000

    First commit
    Added paket; Added ab3d package; Used biding string

---
+++ HelloWorldAmaro/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace HelloWorldAmaro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += new RoutedEventHandler(Page_Loaded);
+        }
+
+        void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Amaro amaro = new Amaro { HelloText = "Eu sou crazy" };
+            this.MyGrid.DataContext = amaro;
+        }
+    }
+
+    public class Amaro
+    {
+        public string HelloText { get; set;}
+    }
+}
